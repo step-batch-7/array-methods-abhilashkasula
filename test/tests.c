b1e3a8a1a63924845c5f12b18d54b6f4f1c6dae5
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-05-08 20:23:00 +0530

    added tests for map

--- test/tests.c
+++ test/tests.c
@@ -3,7 +3,7 @@
 
 int square(int number)
 {
-  return 5;
+  return number * number;
 }
 
 void test_map(void)
@@ -12,9 +12,37 @@ void test_map(void)
 
   Array *numbers = create_array(0);
   Array *expected = create_array(0);
+  Array *actual_1 = map(numbers, &square);
+  display_assertion(assert_array(actual_1, expected), "should get empty array for empty array given");
 
-  Array *actual = map(numbers, &square);
-  display_assertion(assert_array(actual, expected), "should get empty array for empty array given");
+  Array *numbers_2 = create_array(1);
+  numbers_2->array[0] = 2;
+  Array *actual_2 = map(numbers_2, &square);
+  Array *expected_2 = create_array(1);
+  expected_2->array[0] = 4;
+  display_assertion(assert_array(actual_2, expected_2), "should map a number for one value in the array");
+
+  Array *numbers_3 = create_array(2);
+  numbers_3->array[0] = 2;
+  numbers_3->array[1] = 3;
+  Array *actual_3 = map(numbers_3, &square);
+  Array *expected_3 = create_array(2);
+  expected_3->array[0] = 4;
+  expected_3->array[1] = 9;
+  display_assertion(assert_array(actual_3, expected_3), "should map two numbers for two values in the array");
+
+  Array *numbers_4 = create_array(4);
+  numbers_4->array[0] = 2;
+  numbers_4->array[1] = 3;
+  numbers_4->array[2] = 4;
+  numbers_4->array[3] = 5;
+  Array *actual_4 = map(numbers_4, &square);
+  Array *expected_4 = create_array(4);
+  expected_4->array[0] = 4;
+  expected_4->array[1] = 9;
+  expected_4->array[2] = 16;
+  expected_4->array[3] = 25;
+  display_assertion(assert_array(actual_4, expected_4), "should map all numbers for values more than two in the array");
 }
 
 int main(void)
