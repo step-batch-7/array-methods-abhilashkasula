afbfcc8f20d803700e27017de43b31b4e6aca7d9
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-05-08 20:46:46 +0530

    added tests for filter

--- test/tests.c
+++ test/tests.c
@@ -6,6 +6,11 @@ int square(int number)
   return number * number;
 }
 
+Bool is_even(int number)
+{
+  return number % 2 == 0;
+}
+
 void test_map(void)
 {
   printf("map\n");
@@ -45,9 +50,79 @@ void test_map(void)
   display_assertion(assert_array(actual_4, expected_4), "should map all numbers for values more than two in the array");
 }
 
+void test_filter(void)
+{
+  printf("filter\n");
+
+  Array *numbers_1 = create_array(0);
+  Array *actual_1 = filter(numbers_1, &is_even);
+  display_assertion(assert_array(actual_1, numbers_1), "should give empty array for empty array given");
+
+  Array *numbers_2 = create_array(1);
+  numbers_2->array[0] = 2;
+  Array *actual_2 = filter(numbers_2, &is_even);
+  Array *expected_2 = create_array(1);
+  expected_2->array[0] = 2;
+  char message_2[] = "should filter one element when only one value that satisfy is given";
+  display_assertion(assert_array(actual_2, expected_2), message_2);
+
+  Array *numbers_3 = create_array(1);
+  numbers_3->array[0] = 3;
+  Array *actual_3 = filter(numbers_3, &is_even);
+  Array *expected_3 = create_array(0);
+  char message_3[] = "should filter no elements when only one value that don't satisfy is given";
+  display_assertion(assert_array(actual_3, expected_3), message_3);
+
+  Array *numbers_4 = create_array(2);
+  numbers_4->array[0] = 2;
+  numbers_4->array[1] = 3;
+  Array *actual_4 = filter(numbers_4, &is_even);
+  Array *expected_4 = create_array(1);
+  expected_4->array[0] = 2;
+  char message_4[] = "should filter the elements that satisfy for two elements in the list";
+  display_assertion(assert_array(actual_4, expected_4), message_4);
+
+  Array *numbers_5 = create_array(4);
+  numbers_5->array[0] = 2;
+  numbers_5->array[1] = 3;
+  numbers_5->array[2] = 4;
+  numbers_5->array[3] = 5;
+  Array *actual_5 = filter(numbers_5, &is_even);
+  Array *expected_5 = create_array(2);
+  expected_5->array[0] = 2;
+  expected_5->array[1] = 4;
+  char message_5[] = "should filter the elements that satisfy for more than two elements in the list";
+  display_assertion(assert_array(actual_5, expected_5), message_5);
+
+  Array *numbers_6 = create_array(4);
+  numbers_6->array[0] = 2;
+  numbers_6->array[1] = 4;
+  numbers_6->array[2] = 6;
+  numbers_6->array[3] = 8;
+  Array *actual_6 = filter(numbers_6, &is_even);
+  Array *expected_6 = create_array(4);
+  expected_6->array[0] = 2;
+  expected_6->array[1] = 4;
+  expected_6->array[2] = 6;
+  expected_6->array[3] = 8;
+  char message_6[] = "should filter all the elements when all the elements satisfy";
+  display_assertion(assert_array(actual_6, expected_6), message_6);
+
+  Array *numbers_7 = create_array(4);
+  numbers_7->array[0] = 1;
+  numbers_7->array[1] = 3;
+  numbers_7->array[2] = 5;
+  numbers_7->array[3] = 7;
+  Array *actual_7 = filter(numbers_7, &is_even);
+  Array *expected_7 = create_array(0);
+  char message_7[] = "should filter no elements when all the elements doesn't satisfy";
+  display_assertion(assert_array(actual_7, expected_7), message_7);
+}
+
 int main(void)
 {
   test_map();
+  test_filter();
   display_passing_count();
   return 0;
 }
